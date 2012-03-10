c85b2387b16b82c0c7ba3a09eaf0173f1203556d
Eugene Luckyanets <leugenea@gmail.com>
2012-03-11 00:32:21 +0400

    myreverse: simple struct

---
+++ myreverse/myreverse.c
@@ -0,0 +1,21 @@
+#define BUFF_SIZE 10
+
+typedef struct
+{
+  int size;
+  char* chars;
+} Buffer;
+
+Buffer* newBuff
+{
+  buff* = malloc(sizeof);
+  buff->chars = malloc(BUFF_SIZE);
+  buff->size = 0;
+  
+  return buff;
+}
+
+int main()
+{
+  
+}
